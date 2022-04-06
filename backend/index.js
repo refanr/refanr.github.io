0d71cdd23b3bc4a135c7181de16f7240225f6a7f
refanr <reynirg20@ru.is>
2022-04-06 21:57:51 +0000

    backend up and running

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,40 @@
+//Sample for Assignment 3
 const express = require('express');
+
+//Import a body parser module to be able to access the request body as json
 const bodyParser = require('body-parser');
+
+//Use cors to avoid issues with testing on localhost
 const cors = require('cors');
 const { type } = require('express/lib/response');
+
 const app = express();
 
-const apiVersion = 'api/v1';
+const apiVersion = '/api/v1';
 
+//Port environment variable already set up to run on Heroku
 let port = process.env.PORT || 3000;
 
+//Tell express to use the body parser module
 app.use(bodyParser.json());
-app.use(cors());
+
+//Tell express to use cors -- enables CORS for this backend
+app.use(cors());  
+
+//Set Cors-related headers to prevent blocking of local requests
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-});
+    next();
+  });
 
 let success = [
     { message: 'Great Success!!'}
 ];
 
 
-app.get('/', (req, res) => {
-    res.status(200).json(success);
+app.get(apiVersion + '/', (req,res) => {
+    res.status(200).send('Great Success!!');
 });
 
 app.listen(port, () => {
